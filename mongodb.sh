57f53742ce2d6911d2f8096f6398b04daa6fbed4
s viswanath <s.viswanath57@gmail.com>
2023-12-21 01:37:21 +0530

    devops practice

--- mongodb.sh
+++ mongodb.sh
@@ -21,6 +21,14 @@ VALIDATE(){
     fi
 }
 
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
 cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 VALIDATE $? "mongo.repo copy"
 
